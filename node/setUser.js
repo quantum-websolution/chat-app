66f4bb45ea0b54fa811fe61e3ac9ae607d5d4f04
tataboo <tatuya.9712@gmail.com>
2023-12-17 17:00:21 +0900

    治ったンゴ

--- node/setUser.js
+++ node/setUser.js
@@ -1,16 +1,20 @@
-var pool = require('./dbConnection');
+const pool = require('./dbConnection');
 
-module.exports = function (req, res, next) {
-    var userId = req.session.user_id;
-    if (userId) {
-        var query = 'SELECT user_id, user_name FROM users WHERE user_id = ' + userId;
-        pool.connect(function (err, connection) {
-            connection.query(query, function (err, rows) {
-                if (!err) {
-                    res.locals.user = rows.rows.length ? rows.rows[0] : false;
-                }
-            });
-        });
+module.exports = async (req, res, next) => {
+  const userId = req.session.user_id;
+
+  if (userId) {
+    try {
+      const query = 'SELECT user_id, user_name FROM users WHERE user_id = $1';
+      const result = await pool.query(query, [userId]);
+
+      if (result.rows.length) {
+        res.locals.user = result.rows[0];
+      }
+    } catch (error) {
+      console.error(error);
     }
-    next();
+  }
+
+  next();
 };
